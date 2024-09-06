5cfe1758def3bc6908e70a32cec01ef8e7881549
TobiasLoga <tobias.loga@web.de>
2024-09-06 18:18:27 +0200

    > Supplement of "data.R"

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Chart settings heat need
+#'
+"ChartSettings_HeatNeed"
+
+#' Chart settings final energy 
+#'
+"ChartSettings_FinalEnergy"
+
+#' Chart settings expectation ranges
+#'
+"ChartSettings_ExpectationRanges"
+
+
+#' Chart data template heat need
+#'
+"ChartData_HeatNeed"
+
+#' Chart data template final energy 
+#'
+"ChartData_FinalEnergy"
+
+#' Chart data template expectation ranges
+#'
+"ChartData_ExpectationRanges"
+
